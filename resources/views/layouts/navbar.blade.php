b007b003d405dd7b69acbf2e8a046cd11159b3f0
raaf17 <raafimuhammadd123@gmail.com>
2024-06-09 18:47:46 +0700

    memperbaiki judul halaman log activity dan navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -6,13 +6,17 @@
         </ul>
     </form>
     <ul class="navbar-nav navbar-right">
-        <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-            <img alt="image" src="{{ asset('images/2.jpg') }}" class="rounded-circle mr-1">
-            <div class="d-sm-none d-lg-inline-block"><strong>Selamat Datang, {{ auth()->user()->username }}</strong></div></a>
+        <li class="dropdown">
+            <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <div class="d-sm-none d-lg-inline-block mr-2"> <b>Selamat Datang, {{ auth()->user()->username }}</b></div>
+                <img class="img-profile rounded-circle" src="{{ asset('images/2.jpg') }}">
+            </a>
             <div class="dropdown-menu dropdown-menu-right">
-                <a href="{{ route('profile') }}" class="dropdown-item has-icon"><i class="far fa-user"></i> Profile</a>
-                <div class="dropdown-divider"></div>
-                <button type="button" class="dropdown-item has-icon text-danger" onclick="document.location.href='{{ route('logout') }}'"><i class="fas fa-sign-out-alt mt-2"></i> <b>Logout</b></button>
+                <form class="form">
+                    <a href="{{ route('profile') }}" class="dropdown-item has-icon"><i class="far fa-user"></i> Profile</a>
+                    <div class="dropdown-divider"></div>
+                    <button type="button" class="dropdown-item has-icon text-danger" style="display: flex; align-items: center" onclick="document.location.href='{{ route('logout') }}'"><i class="fas fa-sign-out-alt mt-2"></i> <b>Logout</b></button>
+                </form>
             </div>
         </li>
     </ul>
